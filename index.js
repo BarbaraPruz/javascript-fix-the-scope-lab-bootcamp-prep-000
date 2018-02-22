862ea621d701faddfc80e13e51ab3554079a2afa
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-02-22 23:02:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function yourAnimal() {
 
 function add2(n) {
 
-
   // Feel free to move things around!
-  const two = 2  return n + two
+  const two = 2 
+  return n + two
 }
 
 var funkyFunction = function() {
